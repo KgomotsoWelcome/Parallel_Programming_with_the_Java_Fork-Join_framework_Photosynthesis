8c71e5eaf2915ff20fd53e17230963f9f83ab0b3
Kgomotso Welcome <kgomotsowelcome02@gmail.com>
2018-09-10 23:30:18 +0200

    Editing Treesum.java

--- TreeSum.java
+++ TreeSum.java
@@ -5,21 +5,27 @@ import java.io.FileNotFoundException;
 public class TreeSum{
 	public static void main(String []args) throws FileNotFoundException{
 		
+		/**reading from input file and inserting 
+		 * data into a newly created 2D array. 
+		 */
 			File filename = new File("sample_input.txt");
 			Scanner scanfile = new Scanner(filename);
-			
 			
 			String textnum = scanfile.nextLine();
 			String[] arraySize = textnum.split(" ");
 			
-			int x = Integer.parseInt(arraySize[0]);
-			int y = Integer.parseInt(arraySize[1]);
+			int arraySizeX = Integer.parseInt(arraySize[0]);
+			int arraySizeY = Integer.parseInt(arraySize[1]);
 			
-			System.out.println(x);
-			System.out.println(y);
+			//Size of Array.
+			System.out.println(arraySizeX);
+			System.out.println(arraySizeY);
 			
-			int dataArray [][] = new int [x][y];
+			int dataArray [][] = new int [arraySizeX][arraySizeY];
 			
+			//printing out the number of trees.
+			System.out.println("Number of trees: "+scanfile.nextLine());
+			//inserting data into array.
 			while (scanfile.hasNextLine()){
 				scanfile.nextLine();
 				System.out.println(scanfile.nextLine());
